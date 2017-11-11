f09d3b9fc59b8b9c03b212d684440fe4b6c5b494
rafagarciac <32666357+rafagarciac@users.noreply.github.com>
2017-11-11 16:10:14 +0100

    Arreglo la conversion de objeto cuando recogemos la calidad del primer ListView al Segundo y muestro por calidades. Arreglo foto de Tesla // Descarga (Estaban cambiadas). y  muestro el ListViewCartas de manera mas ordenada y junta.

--- app/src/main/java/com/example/jorge/clashroyale/ListaEleccionActivity.java
+++ app/src/main/java/com/example/jorge/clashroyale/ListaEleccionActivity.java
@@ -3,9 +3,11 @@ package com.example.jorge.clashroyale;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,7 +24,6 @@ public class ListaEleccionActivity extends AppCompatActivity {
         listaCalidades.add("Normal"); listaCalidades.add("Especial"); listaCalidades.add("Epica"); listaCalidades.add("Legendaria");
         lvTipos = (ListView)findViewById(R.id.listViewTipo);
 
-
         AdaptadorPrimeraLista myAdaptor = new AdaptadorPrimeraLista(getApplicationContext(), listaCalidades);
         lvTipos.setAdapter(myAdaptor);
 
@@ -31,6 +32,8 @@ public class ListaEleccionActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
                 String calidad = (String) adapterView.getItemAtPosition(i);
+                //MUESTRO POR CONSOLA --> SI COGE LA CALIDAD!
+                Log.i("********", calidad);
                 Intent e = new Intent(getApplicationContext(), ListaCartasActivity.class);
                 e.putExtra("calidad", calidad);
                 startActivity(e);
